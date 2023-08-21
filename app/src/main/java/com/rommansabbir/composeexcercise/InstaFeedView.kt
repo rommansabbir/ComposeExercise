c47acaea507202e73f592ea004132d00e108168c
Romman Sabbir <rommansabbir@gmail.com>
2023-08-21 16:16:43 +0600

    MainActivity : show demo preview

--- app/src/main/java/com/rommansabbir/composeexcercise/InstaFeedView.kt
+++ app/src/main/java/com/rommansabbir/composeexcercise/InstaFeedView.kt
@@ -2,10 +2,15 @@ package com.rommansabbir.composeexcercise
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.rommansabbir.composeexcercise.views.BrandHeaderView
+import com.rommansabbir.composeexcercise.views.InstaPostedImageView
+import com.rommansabbir.composeexcercise.views.InstaPostedImageViewDataModel
 import com.rommansabbir.composeexcercise.views.InstaStoriesView
 import com.rommansabbir.composeexcercise.views.InstaStoriesViewDataModel
 
@@ -15,18 +20,50 @@ fun InstaFeedView() {
         //Insta view column
         BrandHeaderView("Banglagram") {
         }
-        val list = mutableListOf<InstaStoriesViewDataModel>().apply {
-            for (index in 0..20) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
+        ) {
+            val list = mutableListOf<InstaStoriesViewDataModel>().apply {
+                for (index in 0..20) {
+                    add(
+                        InstaStoriesViewDataModel(
+                            index, "Test $index", R.drawable.baseline_account_circle_24
+                        )
+                    )
+                }
+            }
+            InstaStoriesView(list = list, onCallback = {
+
+            })
+
+            val models = mutableListOf<InstaPostedImageViewDataModel>().apply {
                 add(
-                    InstaStoriesViewDataModel(
-                        index, "Test $index", R.drawable.baseline_account_circle_24
+                    InstaPostedImageViewDataModel(
+                        0, "Test $0", R.drawable.baseline_account_circle_24
+                    )
+                )
+                add(
+                    InstaPostedImageViewDataModel(
+                        1, "Test $1", R.drawable.baseline_account_circle_24
+                    )
+                )
+                add(
+                    InstaPostedImageViewDataModel(
+                        2, "Test $2", R.drawable.baseline_account_circle_24
+                    )
+                )
+                add(
+                    InstaPostedImageViewDataModel(
+                        3, "Test $3", R.drawable.baseline_account_circle_24
                     )
                 )
             }
+            repeat(models.size) { index ->
+                InstaPostedImageView(models[index])
+            }
         }
-        InstaStoriesView(list = list, onCallback = {
-
-        })
     }
 }
 
--- app/src/main/java/com/rommansabbir/composeexcercise/MainActivity.kt
+++ app/src/main/java/com/rommansabbir/composeexcercise/MainActivity.kt
@@ -20,9 +20,8 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    InstaFeedView()
                 }
             }
         }
