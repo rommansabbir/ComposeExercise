18194ac2714e129cb47362c231d04f2fde9dba96
Romman Sabbir <rommansabbir@gmail.com>
2023-08-23 12:19:57 +0600

    story view : refactored

--- app/src/main/java/com/rommansabbir/composeexcercise/InstaFeedView.kt
+++ app/src/main/java/com/rommansabbir/composeexcercise/InstaFeedView.kt
@@ -4,17 +4,43 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.rommansabbir.composeexcercise.views.BrandHeaderView
+import com.rommansabbir.composeexcercise.views.InstaPostedContentLikeCommentsViewActions
 import com.rommansabbir.composeexcercise.views.InstaPostedImageView
 import com.rommansabbir.composeexcercise.views.InstaPostedImageViewDataModel
 import com.rommansabbir.composeexcercise.views.InstaStoriesView
 import com.rommansabbir.composeexcercise.views.InstaStoriesViewDataModel
+
+object MockDataProvider {
+    val getMockResponse by lazy {
+        mutableListOf<InstaPostedImageViewDataModel>().apply {
+            for (index in 0..100) {
+                add(
+                    InstaPostedImageViewDataModel(
+                        index, "Test $index", R.drawable.baseline_account_circle_24
+                    )
+                )
+            }
+        }
+    }
+    val getStories by lazy {
+        mutableListOf<InstaStoriesViewDataModel>().apply {
+            for (index in 0..20) {
+                add(
+                    InstaStoriesViewDataModel(
+                        index, "Test $index", R.drawable.baseline_account_circle_24
+                    )
+                )
+            }
+        }
+    }
+}
 
 @Composable
 fun InstaFeedView() {
@@ -26,46 +52,53 @@ fun InstaFeedView() {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .verticalScroll(rememberScrollState())
+            /*.verticalScroll(rememberScrollState())*/
         ) {
-            val list = mutableListOf<InstaStoriesViewDataModel>().apply {
-                for (index in 0..20) {
-                    add(
-                        InstaStoriesViewDataModel(
-                            index, "Test $index", R.drawable.baseline_account_circle_24
-                        )
-                    )
-                }
-            }
-            InstaStoriesView(list = list, onCallback = {
-                Toast.makeText(context, "Insta story clicked", Toast.LENGTH_SHORT).show()
-            })
 
-            val models = mutableListOf<InstaPostedImageViewDataModel>().apply {
-                add(
-                    InstaPostedImageViewDataModel(
-                        0, "Test $0", R.drawable.baseline_account_circle_24
-                    )
-                )
-                add(
-                    InstaPostedImageViewDataModel(
-                        1, "Test $1", R.drawable.baseline_account_circle_24
-                    )
-                )
-                add(
-                    InstaPostedImageViewDataModel(
-                        2, "Test $2", R.drawable.baseline_account_circle_24
-                    )
-                )
-                add(
-                    InstaPostedImageViewDataModel(
-                        3, "Test $3", R.drawable.baseline_account_circle_24
-                    )
-                )
-            }
-            repeat(models.size) { index ->
-                InstaPostedImageView(models[index])
-            }
+            LazyColumn(content = {
+                item {
+                    LazyRow(content = {
+                        items(count = MockDataProvider.getStories.size, key = {
+                            MockDataProvider.getStories[it].id
+                        }) { index ->
+                            InstaStoriesView(model = MockDataProvider.getStories[index], onCallback = {
+                                Toast.makeText(context, "Insta story clicked", Toast.LENGTH_SHORT).show()
+                            })
+                        }
+                    })
+                }
+
+                items(count = MockDataProvider.getMockResponse.size, key = {
+                    MockDataProvider.getMockResponse[it].id
+                }) { index ->
+                    InstaPostedImageView(MockDataProvider.getMockResponse[index]) {
+                        when (it) {
+                            InstaPostedContentLikeCommentsViewActions.OnSavePost -> {
+                                Toast.makeText(context, "On save post", Toast.LENGTH_SHORT).show()
+                            }
+
+                            InstaPostedContentLikeCommentsViewActions.OnFavorite -> {
+                                Toast.makeText(context, "On favorite post", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+
+                            InstaPostedContentLikeCommentsViewActions.OnOptionSelected -> {
+                                Toast.makeText(context, "On option selected", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+
+                            InstaPostedContentLikeCommentsViewActions.OnShare -> {
+                                Toast.makeText(context, "On share post", Toast.LENGTH_SHORT).show()
+                            }
+
+                            InstaPostedContentLikeCommentsViewActions.OnShowComments -> {
+                                Toast.makeText(context, "On show comments", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+                    }
+                }
+            })
         }
     }
 }
